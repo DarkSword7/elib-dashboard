05546efa74b35e787feeb44891842085f5fdf948
AndroVision <91982670+DarkSword7@users.noreply.github.com>
2024-06-01 15:19:43 +0530

    make routes protected

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,6 +1,12 @@
-import { Outlet } from "react-router-dom";
+import useTokenStore from "@/Store";
+import { Navigate, Outlet } from "react-router-dom";
 
 const AuthLayout = () => {
+  const token = useTokenStore((state) => state.token);
+
+  if (token) {
+    return <Navigate to="/dashboard/home" replace />;
+  }
   return <Outlet />;
 };
 
--- src/layouts/DashboardLayout.tsx
+++ src/layouts/DashboardLayout.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from "react-router-dom";
+import { Link, Navigate, Outlet } from "react-router-dom";
 import {
   Bell,
   CircleUser,
@@ -18,8 +18,15 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Input } from "@/components/ui/input";
+import useTokenStore from "@/Store";
 
 const DashboardLayout = () => {
+  const token = useTokenStore((state) => state.token);
+
+  if (!token) {
+    return <Navigate to="/auth/login" replace />;
+  }
+
   return (
     <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
       <div className="hidden border-r bg-muted/40 md:block">
